b2c634e58968c02849ed95bb17adb74e3b257fae
Chao Xiang <chao.xiang.cs@gmail.com>
2014-05-17 18:01:42 +0800

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc, const char **argv)
 {
     
-    
+    // use communication method to implement data sharing among all processers
     
     
     // never forget to exit system with such code
